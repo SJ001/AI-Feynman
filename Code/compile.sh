1d30568e5d3f0b373370b3586ae91e0c5d9d2a7b
Silviu Marian Udrescu <smu226@nyu.edu>
2020-04-29 14:11:26 -0400

    Add files via upload

--- Code/compile.sh
+++ Code/compile.sh
@@ -1,3 +1,6 @@
 gfortran -ffixed-line-length-none -O3 -o symbolic_regress1.x symbolic_regress1.f
 gfortran -ffixed-line-length-none -O3 -o symbolic_regress2.x symbolic_regress2.f
 gfortran -ffixed-line-length-none -O3 -o symbolic_regress3.x symbolic_regress3.f
+chmod 555 brute_force_oneFile_v1.scr
+chmod 555 brute_force_oneFile_v2.scr
+chmod 555 brute_force_oneFile_v3.scr
