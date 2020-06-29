1caa33dfd08424beca78fcab50dde4149e2fe9f2
Silviu Marian Udrescu <smu226@nyu.edu>
2020-06-28 23:56:02 -0400

    Add files via upload

--- Code/ai_feynman_example.py
+++ Code/ai_feynman_example.py
@@ -1,4 +1,4 @@
 from S_run_aifeynman import run_aifeynman
 
-run_aifeynman("../example_data/","example2.txt",30,"14ops.txt", polyfit_deg=3, NN_epochs=400)
+run_aifeynman("../example_data/","example1.txt",30,"14ops.txt", polyfit_deg=3, NN_epochs=500)
 
