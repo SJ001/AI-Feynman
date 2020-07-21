78da501c84ee9fd6628f659e24723349c2f155eb
Silviu Marian Udrescu <smu226@nyu.edu>
2020-07-21 02:03:19 -0400

    Add files via upload

--- Code/S_add_sym_on_pareto.py
+++ Code/S_add_sym_on_pareto.py
@@ -8,6 +8,7 @@ import os
 from os import path
 from sympy import Symbol, lambdify, N
 from get_pareto import Point, ParetoSet
+from S_get_expr_complexity import get_expr_complexity
 
 def add_sym_on_pareto(pathdir,filename,PA1,idx1,idx2,PA,sym_typ):
     possible_vars = ["x%s" %i for i in np.arange(0,30,1)]
@@ -17,7 +18,8 @@ def add_sym_on_pareto(pathdir,filename,PA1,idx1,idx2,PA,sym_typ):
         for j in range(len(possible_vars)-2,idx2-1,-1):
             exp1 = exp1.replace(possible_vars[j],possible_vars[j+1])
         exp1 = exp1.replace(possible_vars[idx1],"(" + possible_vars[idx1] + sym_typ + possible_vars[idx2] + ")")
-        PA.add(Point(x=float(PA1[i][0]),y=float(PA1[i][1]),data=str(exp1)))
+        compl = get_expr_complexity(exp1)
+        PA.add(Point(x=compl,y=float(PA1[i][1]),data=str(exp1)))
 
     return PA
 
--- Code/S_combine_pareto.py
+++ Code/S_combine_pareto.py
@@ -9,6 +9,7 @@ import os
 from os import path
 from sympy import Symbol, lambdify, N
 from get_pareto import Point, ParetoSet
+from S_get_expr_complexity import get_expr_complexity
 
 def combine_pareto(input_data,PA1,PA2,idx_list_1,idx_list_2,PA,sep_type = "+"):
     possible_vars = ["x%s" %i for i in np.arange(0,30,1)]
@@ -17,7 +18,6 @@ def combine_pareto(input_data,PA1,PA2,idx_list_1,idx_list_2,PA,sep_type = "+"):
     for i in range(len(PA1)):
         for j in range(len(PA2)):
             try:
-                complexity = float(PA1[i][0])+float(PA2[j][0])
                 # replace the variables from the separated parts with the variables reflecting the new combined equation
                 exp1 = PA1[i][2]
                 exp2 = PA2[j][2]
@@ -26,7 +26,8 @@ def combine_pareto(input_data,PA1,PA2,idx_list_1,idx_list_2,PA,sep_type = "+"):
                 for k in range(len(idx_list_2)-1,-1,-1):
                     exp2 = exp2.replace(possible_vars[k],possible_vars[idx_list_2[k]])
                 new_eq = "(" + exp1 + ")" + sep_type + "(" + exp2 + ")"
-                PA.add(Point(x=complexity,y=get_symbolic_expr_error(input_data,new_eq),data=new_eq))
+                compl = get_expr_complexity(new_eq)
+                PA.add(Point(x=compl,y=get_symbolic_expr_error(input_data,new_eq),data=new_eq))
             except:
                 continue
     return PA
