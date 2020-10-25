7b1d65da789fb76ecd799b8f94ce46782333efab
Andrew Tan <andrewtsoc@gmail.com>
2020-10-25 12:37:49 -0400

    Bumped version to 2.0.7.

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='aifeynman',
-    version='2.0.6',
+    version='2.0.7',
     description='AI Feynman: a Physics-Inspired Method for Symbolic Regression',
     long_description=long_description,
     long_description_content_type="text/markdown",
